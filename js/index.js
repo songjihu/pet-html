f4a21b3fef58cce9f8c22ed74b467a4cc89cc205
Lee_JH <41672786+iamlijinhui@users.noreply.github.com>
2019-06-06 22:31:52 +0800

    上传index.js news,js

---
+++ js/index.js
@@ -0,0 +1,43 @@
+App = {
+ 
+
+  init: async function() {
+    // Load pets.
+	
+    $.getJSON('news.json', function(data) {
+		
+      var petsRow = $('#news-title');
+      var petTemplate = $('#body');
+
+      for (i = 0; i < data.length; i ++) {
+				
+				petTemplate.find('img').attr('src', data[i].image);
+        petTemplate.find('.news-title').text(data[i].tltle);       
+        petTemplate.find('.news-outline').text(data[i].outline);
+        petTemplate.find('.news-author').text(data[i].author);
+        petTemplate.find('.news-time').text(data[i].time);
+				petTemplate.find('.btn').attr('value', data[i].id);
+				 
+        
+        petsRow.append(petTemplate.html());
+      }
+			
+			 $(".col-md-12").on("click",".btn",function(){
+				 alert(this.value);
+				 document.cookie=this.value;
+				 window.open("news.html");
+			});
+    });
+  
+    return false;
+  }
+}
+$(function() {
+  $(window).load(function() {
+  App.init();
+	
+	});
+
+  });
+	
+
---
+++ js/news.js
@@ -0,0 +1,44 @@
+App = {
+ 
+
+  init: async function() {
+    // Load pets.
+	var id=document.cookie;
+
+    $.getJSON('news.json', function(data) {
+      var petsRow = $('#news');
+      var petTemplate = $('#body');
+// 
+// 				
+
+				 petTemplate.find('img').attr('src', data[id].image);
+				 petTemplate.find('.img-circle').attr('src', data[id].image);
+         petTemplate.find('.text-center').text(data[id].tltle);
+       
+         // petTemplate.find('.news-outline').text(data[id].outline);
+         // petTemplate.find('.news-author').text(data[id].author);
+         petTemplate.find('.txt').text(data[id].body);
+				
+// // 
+//         
+        petsRow.append(petTemplate.html());
+// 			
+// 			 $(".col-md-12").on("click",".btn",function(){
+// 				 alert(this.value);
+// 				 document.cookie=this.value;
+// 				 window.open("news.html");
+			// });
+    });
+  
+    return false;
+  }
+}
+$(function() {
+  $(window).load(function() {
+  App.init();
+	
+	});
+
+  });
+	
+
